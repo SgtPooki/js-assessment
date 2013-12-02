8419dd09c4edeefd89de2a0abef3e1f1b758ea54
Russell Dempsey <SgtPooki@gmail.com>
2013-12-02 16:57:52 -0600

    finished objects and context

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+        return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+        constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+        var has = {}.hasOwnProperty.bind(obj);
+        var result = [];
+        for(prop in obj) {
+            if (has(prop)) {
+                result.push(prop + ': ' + obj[prop]);
+            }
+        }
+        return result;
     }
   };
 });
