1fb19e4d3d8fad394e820377846fb44fc7d2b45e
Russell Dempsey <SgtPooki@gmail.com>
2013-12-02 17:22:01 -0600

    finished the module pattern

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+        var C = function(greeting, name) {
+            this.greeting = greeting;
+            this.name = name;
+        };
+        C.prototype.sayIt = function() {
+            return this.greeting + ', ' + this.name;
+        };
+        
+        return new C(str1, str2);
     }
   };
 });
