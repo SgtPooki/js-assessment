673b55e7458665ec06039ff8c47de308367bdeb3
Russell Dempsey <SgtPooki@gmail.com>
2013-12-04 11:01:08 -0600

    completed async behavior tests

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+        var deferred = $.Deferred();
+        deferred.resolve(value);
+        return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
-
+        var deferred = $.Deferred(); 
+        $.get(url, null, function(data) {
+            var result = data.people.map(function(o,i){
+                return o.name;
+            });
+            result.sort();
+            deferred.resolve(result);
+        }, 'json');
+        return deferred.promise();
     }
   };
 });
