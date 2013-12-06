dd985a469743d2ca0123349aba363b7af001a773
Russell Dempsey <SgtPooki@gmail.com>
2013-12-06 10:21:42 -0600

    completed binary operators

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        var bin = this.convertToBinary(num);
+        return parseInt(bin[bin.length - bit]);
+        
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+        var result = num.toString(2);
+        while (result.length % 8 !== 0) {
+            result = '0' + result;
+        }
+        return result;
     },
 
     multiply: function(a, b) {
